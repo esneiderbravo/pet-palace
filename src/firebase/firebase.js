d45774e1f73a143750baf6e325b7c993f8f32458
Esneider Bravo <esneiderbravoyb@gmail.com>
2023-06-10 22:28:07 -0500

    PUT CREDENTIALS IN ENV FILE

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBEb6kHyMuanToVANmpp8ELyqXbDQP5BRg',
-  authDomain: 'pet-palace-4c65f.firebaseapp.com',
-  projectId: 'pet-palace-4c65f',
-  storageBucket: 'pet-palace-4c65f.appspot.com',
-  messagingSenderId: '582050939183',
-  appId: '1:582050939183:web:ed03e6a42ffe844faaa1da',
-  measurementId: 'G-FGM43NZ371',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
